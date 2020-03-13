1c9a86107a11f4cfcb8b20940ecfc108c6c0eabb
Arnas Plytnikas <plytnikas99@gmail.com>
2020-03-13 16:11:06 +0200

    Second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+      { path: 'register', component: RegisterPageComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
