6ac5e5e4f0d70a02909b9feb346c241152a3f57f
Ingvaras Galinskas <ingvaraz4@gmail.com>
2020-03-16 18:02:38 +0200

    small fix

--- src/app/services/application.service.spec.ts
+++ src/app/services/application.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
 import { ApplicationService } from './application.service';
 
 describe('ApplicationService', () => {
