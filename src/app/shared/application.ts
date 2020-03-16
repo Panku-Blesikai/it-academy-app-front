b08fde768abdfe0d4a922bc328ecfd3bd586ac3f
Ingvaras Galinskas <ingvaraz4@gmail.com>
2020-03-16 15:16:14 +0200

    Started connecting BE and FE

---
+++ src/app/shared/application.ts
@@ -0,0 +1,13 @@
+export interface Application {
+  id: string;
+  email: string;
+  education: string;
+  name: string;
+  surname: string;
+  tel: string;
+  answerContract: string;
+  answerTime: string;
+  answerMotivation: string;
+  answerExperience: string;
+  answerInfoAbout: string;
+}
