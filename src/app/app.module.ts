7532e5c3885d9249c4a9dac7ad3c9f50b5300b21
kotryna <kotryna.slepetyte@gmail.com>
2020-03-26 19:00:20 +0200

    conflicts solved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { ApplicationsListComponent } from './applications-list/applications-list.component';
-import { ApplicationComponent } from './application/application.component';
-import { ApplicationsListPageComponent } from './applications-list-page/applications-list-page.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { RegisterPageComponent } from './register-page/register-page.component';
-import { RegisterFormComponent } from './register-form/register-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { ApplicationPageComponent } from './application-page/application-page.component';
-import { AboutPageComponent } from './about-page/about-page.component';
-import { SuccessRegistrationPageComponent } from './success-registration-page/success-registration-page.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {ApplicationsListComponent} from './applications-list/applications-list.component';
+import {ApplicationComponent} from './application/application.component';
+import {ApplicationsListPageComponent} from './applications-list-page/applications-list-page.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {RegisterPageComponent} from './register-page/register-page.component';
+import {RegisterFormComponent} from './register-form/register-form.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {ApplicationPageComponent} from './application-page/application-page.component';
+import {AboutPageComponent} from './about-page/about-page.component';
+import {SuccessRegistrationPageComponent} from './success-registration-page/success-registration-page.component';
+import {TextareaAutosizeModule} from "ngx-textarea-autosize";
 
 @NgModule({
   declarations: [
@@ -34,9 +35,11 @@ import { SuccessRegistrationPageComponent } from './success-registration-page/su
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    TextareaAutosizeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
