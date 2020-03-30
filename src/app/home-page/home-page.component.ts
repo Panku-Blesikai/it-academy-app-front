552eb2528e8732b6b1ce715df82f9c570262e87d
Ingvaras Galinskas <ingvaraz4@gmail.com>
2020-03-30 11:43:15 +0300

    loader fix

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(){
+    this.hideLoader();
+  }
+
+  hideLoader(): void {
+    document.getElementById('loader').style.display = 'none';
+    document.getElementById('home-page').style.display = 'block';
+  }
 }
