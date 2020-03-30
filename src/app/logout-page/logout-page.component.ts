a7292d363ab2535f42e08381fe5ee50c2e58b2fe
Arsenij <arsendk@gmail.com>
2020-03-30 15:04:46 +0300

    menu bar for admin changed

--- src/app/logout-page/logout-page.component.ts
+++ src/app/logout-page/logout-page.component.ts
@@ -17,7 +17,7 @@ export class LogoutPageComponent implements OnInit {
 
   ngOnInit() {
     this.authenticationService.logout();
-    this.router.navigate(['/home']);
+    this.router.navigate(['/login']);
   }
 
 }
