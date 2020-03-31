46b3430ebfc979a4ffa908076539539d651e08ee
Ingvaras Galinskas <ingvaraz4@gmail.com>
2020-03-31 14:51:47 +0300

    advanced filters

--- src/app/applications-list/applications-list.component.ts
+++ src/app/applications-list/applications-list.component.ts
@@ -13,6 +13,9 @@ export class ApplicationsListComponent implements OnInit {
   public applications: Application[];
   private statusInput: string;
   private searchFieldInput: string;
+  private availabilityInput: string;
+  private agreementInput: string;
+  private showAdvancedFilters: boolean;
 
   constructor(private sortingService: SortingService, private filterService: FilterService) { }
 
@@ -26,8 +29,35 @@ export class ApplicationsListComponent implements OnInit {
     this.filter();
   }
 
+  updateAvailabilityInput(availability: string) {
+    this.availabilityInput = availability;
+    this.filter();
+  }
+
+  updateAgreementInput(agreement: string) {
+    this.agreementInput = agreement;
+    this.filter();
+  }
+
   filter() {
-    this.applications = this.filterService.filterBy(this.allApplications, this.searchFieldInput, this.statusInput);
+    this.applications = this.filterService.filterBy(
+      this.allApplications, this.searchFieldInput, this.statusInput, this.availabilityInput, this.agreementInput
+    );
+  }
+
+  advancedFilters() {
+    this.showAdvancedFilters = !this.showAdvancedFilters;
+    if (this.showAdvancedFilters) {
+      document.getElementById('show-advanced').innerHTML = '' +
+        '<i class="fas fa-caret-down"></i>\n' +
+        'Slėpti papildomus filtrus';
+      document.getElementById('advanced-filters').style.display = 'block';
+    } else {
+      document.getElementById('show-advanced').innerHTML = '' +
+        '<i class="fas fa-caret-right"></i>\n' +
+        'Rodyti papildomus filtrus';
+      document.getElementById('advanced-filters').style.display = 'none';
+    }
   }
 
   sort(propertyName: string) {
@@ -39,5 +69,8 @@ export class ApplicationsListComponent implements OnInit {
     this.applications = this.allApplications;
     this.statusInput = 'VISI';
     this.searchFieldInput = '';
+    this.availabilityInput = 'VISI';
+    this.agreementInput = 'VISI';
+    this.showAdvancedFilters = false;
   }
 }
