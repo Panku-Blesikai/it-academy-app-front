61245fc8d0f1013ca49fa19e94f01e219c24c983
Kotryna Šlepetytė <49883485+sniegapele@users.noreply.github.com>
2020-04-08 22:10:46 +0300

    email regex fix

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -81,7 +81,7 @@ export class RegisterFormComponent implements OnInit {
     }],
     email: ['', {
       validators: [Validators.required, Validators.maxLength(256),
-        Validators.pattern(`^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$`)],
+        Validators.pattern(`^[a-zA-Z0-9_+&*-]+(?:\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,7}$`)],
       updateOn: 'blur'
     }],
     phone: ['', {
