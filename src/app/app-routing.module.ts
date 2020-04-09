60ef051f04d938a1f0ed73b5a8d1995c217a2a18
kotryna <kotryna.slepetyte@gmail.com>
2020-04-09 15:42:49 +0300

    page scrolling bug fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,37 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
-import { RegisterPageComponent } from './register-page/register-page.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomePageComponent} from './home-page/home-page.component';
+import {RegisterPageComponent} from './register-page/register-page.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 import {ApplicationsListPageComponent} from './applications-list-page/applications-list-page.component';
 import {ApplicationPageComponent} from './application-page/application-page.component';
-import { AboutPageComponent } from './about-page/about-page.component';
+import {AboutPageComponent} from './about-page/about-page.component';
 import {SuccessRegistrationPageComponent} from './success-registration-page/success-registration-page.component';
 import {LoginPageComponent} from './login-page/login-page.component';
 import {LogoutPageComponent} from './logout-page/logout-page.component';
 
 
 const routes: Routes = [
-  { path: 'home', component: HomePageComponent },
-  { path: 'register', component: RegisterPageComponent },
-  { path: 'about', component: AboutPageComponent },
-  { path: 'application/:idHash', component: ApplicationPageComponent },
-  { path: 'admin', component: ApplicationsListPageComponent },
-  { path: 'register/success', component: SuccessRegistrationPageComponent },
-  { path: 'login', component: LoginPageComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'logout', component: LogoutPageComponent},
-  { path: '**', component: NotFoundComponent }
+  {path: 'home', component: HomePageComponent},
+  {path: 'register', component: RegisterPageComponent},
+  {path: 'about', component: AboutPageComponent},
+  {path: 'application/:idHash', component: ApplicationPageComponent},
+  {path: 'admin', component: ApplicationsListPageComponent},
+  {path: 'register/success', component: SuccessRegistrationPageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'logout', component: LogoutPageComponent},
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      enableTracing: false,
+      scrollPositionRestoration: 'top'
+    })
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { ApplicationsListComponent } from './applications-list/applications-list.component';
-import { ApplicationComponent } from './application/application.component';
-import { ApplicationsListPageComponent } from './applications-list-page/applications-list-page.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { RegisterPageComponent } from './register-page/register-page.component';
-import { RegisterFormComponent } from './register-form/register-form.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {ApplicationsListComponent} from './applications-list/applications-list.component';
+import {ApplicationComponent} from './application/application.component';
+import {ApplicationsListPageComponent} from './applications-list-page/applications-list-page.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {RegisterPageComponent} from './register-page/register-page.component';
+import {RegisterFormComponent} from './register-form/register-form.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import { ApplicationPageComponent } from './application-page/application-page.component';
-import { AboutPageComponent } from './about-page/about-page.component';
-import { SuccessRegistrationPageComponent } from './success-registration-page/success-registration-page.component';
-import { LoginPageComponent } from './login-page/login-page.component';
+import {ApplicationPageComponent} from './application-page/application-page.component';
+import {AboutPageComponent} from './about-page/about-page.component';
+import {SuccessRegistrationPageComponent} from './success-registration-page/success-registration-page.component';
+import {LoginPageComponent} from './login-page/login-page.component';
 import {TextareaAutosizeModule} from 'ngx-textarea-autosize';
-import { LogoutPageComponent } from './logout-page/logout-page.component';
+import {LogoutPageComponent} from './logout-page/logout-page.component';
 import {HttpInterceptorService} from './services/http-interceptor.service';
 import {NgTempusdominusBootstrapModule} from 'ngx-tempusdominus-bootstrap';
-import { FooterComponent } from './footer/footer.component';
+import {FooterComponent} from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -54,4 +54,5 @@ import { FooterComponent } from './footer/footer.component';
   }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
